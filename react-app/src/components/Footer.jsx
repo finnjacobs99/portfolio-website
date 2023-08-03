69d38dc66bb83fdda1aeb9101fff567e115cb23c
Finn Jacobs <finn.jacobs99@gmail.com>
2023-08-03 12:59:35 -0400

    changed ordering of footer links

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 
 const Footer = () => {
   return (
@@ -9,25 +9,26 @@ const Footer = () => {
     >
       <span>Developed by Finn Jacobs</span>
       <div className='flex'>
-        <a 
+        <a
           className='flex justify-center items-center mr-4 cursor-pointer transition hover:text-white ease-in-out duration-300'
-          href='https://www.linkedin.com/in/finn-jacobs-5808831bb'
-          target='_blank'
-        >
-          <span>LinkedIn</span>
-          <AiFillLinkedin className='ml-1'/>
-        </a>
-        <a 
-          className='flex justify-center items-center cursor-pointer transition hover:text-white ease-in-out duration-300'
           href='https://github.com/finnjacobs99/portfolio-website'
           target='_blank'
         >
           <span>Github</span>
-          <AiFillGithub className='ml-1'/>
+          <AiFillGithub className='ml-1' />
+        </a>
+
+        <a
+          className='flex justify-center items-center cursor-pointer transition hover:text-white ease-in-out duration-300'
+          href='https://www.linkedin.com/in/finn-jacobs-5808831bb'
+          target='_blank'
+        >
+          <span>LinkedIn</span>
+          <AiFillLinkedin className='ml-1' />
         </a>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
