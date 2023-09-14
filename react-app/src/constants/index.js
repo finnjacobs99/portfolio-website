b1f0802d4ae54fa8d58015b910a9bc92ecde4756
Finn Jacobs <finn.jacobs99@gmail.com>
2023-09-14 15:09:44 -0400

    revised content

--- react-app/src/constants/index.js
+++ react-app/src/constants/index.js
@@ -7,7 +7,7 @@ export const projectList = [
     img: velocikeyImg,
     title: 'Velocikey',
     description:
-      'A typing test platform that combines a minimalist design with customizable features. Velocikey lets you tailor your experience with variable test lengths and multiple color themes. Witness your improvement over time with personal result tracking. Engage in competition as you see how you rank against other users and strive to reach the top of the global leaderboard.',
+      'A typing test platform that combines a minimalist design with customizable features. Witness your improvement over time with personal result tracking. Engage in competition as you see how you rank against other users and strive to reach the top of the global leaderboard.',
     demo: '',
     website: 'https://velocikey.vercel.app/',
     code: 'https://github.com/finnjacobs99/velocikey',
@@ -17,7 +17,7 @@ export const projectList = [
     id: 'project-2',
     img: wasteWatcherImg,
     title: 'Waste Watcher UI',
-    description: `A cross platform mobile application for tracking household food waste. The Waste Watcher user interface was derived from a multitude of requirements gathered from the University of Maine Mitchell Center for Sustainability Solutions. It presents an array of features ranging from waste tracking and habit analysis/visualization, to account customization and leaderboards ranking user's waste reduction.`,
+    description: `A cross platform mobile application for tracking household food waste. The Waste Watcher user interface was derived from a multitude of requirements gathered from the University of Maine Mitchell Center for Sustainability Solutions. It presents an array of features ranging from waste tracking and visualization, to account customization and leaderboards ranking user's waste reduction.`,
     demo: 'https://www.youtube.com/watch?v=nh34OQHx2WI',
     website: '',
     code: 'https://github.com/finnjacobs99/Waste-Watcher',
